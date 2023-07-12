cb0b734bc6551ef06eaa1ea58cf05d720f072c46
Tomas <tfhalloween@gmail.com>
2023-07-12 17:34:47 +0300

    - Created Products interface. - Fixed type in json category. - DataService can now fetch products, with errorHandling.

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, of, retry, take, tap } from 'rxjs';
+import { IProduct } from '../types/product.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  private productsUrl = 'http://localhost:3000/products';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.productsUrl).pipe(
+      tap((data) => console.log(data)),
+      take(1),
+      retry(1),
+      catchError(this.handleError<IProduct[]>('getProducts', []))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(operation, error);
+
+      return of(result as T);
+    };
+  }
 }
---
+++ src/app/core/types/product.interface.ts
@@ -0,0 +1,27 @@
+export type ProductCategory =
+  | 'all-day_breakfast'
+  | 'cold_appetizers'
+  | 'homemade_meat_delicacies'
+  | 'ukrainian_pickles'
+  | 'salads'
+  | 'soups'
+  | 'hot_appetizers'
+  | 'traditional_deruny'
+  | 'dumplings'
+  | 'meat_dishes'
+  | 'fish_dishes'
+  | 'side_dishes'
+  | 'open_fire_dishes'
+  | 'shashlik'
+  | 'lyulya-kebab'
+  | 'sauces'
+  | 'bread_basket'
+  | 'dessert';
+
+export interface IProduct {
+  id: number;
+  price: string;
+  name: string;
+  description: string;
+  category: ProductCategory;
+}
