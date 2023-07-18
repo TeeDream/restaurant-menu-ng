5671397f2be69f44368efd1a2b5be27ed43221d6
Tomas <tfhalloween@gmail.com>
2023-07-18 17:57:39 +0300

    - Copied basic styles for register-page. - Added routes and links.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { MaterialModule } from '@src/app/shared/material.module';
 import { SharedModule } from '@src/app/shared/shared.module';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
   declarations: [LoginPageComponent, RegisterPageComponent],
-  imports: [CommonModule, MaterialModule, SharedModule],
+  imports: [CommonModule, MaterialModule, SharedModule, RouterLink],
 })
 export class AuthModule {}
--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@src/app/auth/services/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.scss']
+  styleUrls: ['./register-page.component.scss'],
 })
-export class RegisterPageComponent {
+export class RegisterPageComponent implements OnInit {
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
+  loginGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+  });
+
+  // getErrorMessage() {
+  //   if (this.loginGroup.value.email.hasError('required')) {
+  //     return 'You must enter a value';
+  //   }
+  //
+  //   return this.loginGroup.value.email.hasError('email') ? 'Not a valid email' : '';
+  // }
+
+  public onSubmit(): void {
+    // this.router.routerState.snapsho.t
+
+    if (
+      !this.loginGroup.valid ||
+      !(this.loginGroup.value.email && this.loginGroup.value.password)
+    )
+      return;
+
+    this.auth.registerUser({
+      email: this.loginGroup.value.email,
+      password: this.loginGroup.value.password,
+      name: 'Tomas',
+    });
+    // .subscribe((data) => console.log(data));
+  }
+
+  ngOnInit(): void {
+    console.log(this.route.snapshot.paramMap.get('id'));
+  }
 }
